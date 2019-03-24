bee3500343ef596946d2da47e31029871a68eb4c
zhoulibo1988 <zhoulibo_vip@163.com>
2019-03-24 17:10:49 +0800

    ff

--- quartz/src/main/java/com/quartz/QuartzApplication.java
+++ quartz/src/main/java/com/quartz/QuartzApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 * @Description: 定时任务调度中心
 * @author Mr.zhou 
 * @date Mar 24, 2019 5:08:17 PM
+* 
  */
 @SpringBootApplication
 public class QuartzApplication {
