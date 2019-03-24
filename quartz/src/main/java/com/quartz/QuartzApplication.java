6632a808021d7a50be2e8488d0da9ef60057f696
zhoulibo1988 <zhoulibo_vip@163.com>
2019-03-24 17:09:14 +0800

    update

--- quartz/src/main/java/com/quartz/QuartzApplication.java
+++ quartz/src/main/java/com/quartz/QuartzApplication.java
@@ -2,7 +2,12 @@ package com.quartz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+* @ClassName: QuartzApplication 
+* @Description: 定时任务调度中心
+* @author Mr.zhou 
+* @date Mar 24, 2019 5:08:17 PM
+ */
 @SpringBootApplication
 public class QuartzApplication {
 
